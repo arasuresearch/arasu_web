374fdfe7d5e4d847ba8cec08d568b454c8cb1da7
JudeArasu <judearasu@gmail.com>
2015-01-02 01:23:15 -0500

    basic structure applied

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name arasuWebApp
+ * @description
+ * # arasuWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('arasuWebApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name arasuWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the arasuWebApp
+ */
+angular.module('arasuWebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
